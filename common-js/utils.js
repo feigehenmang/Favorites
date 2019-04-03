d13adb10c9b3e1bd8f1b66b92edd6d737db888bf
--global <--global>
2019-04-03 17:55:46 +0800

    add page

--- common-js/utils.js
+++ common-js/utils.js
@@ -1,13 +1,34 @@
 var NiceUtils = (function(){
     return {
-        urlSerialize: function(json){
-            json = json || {};
-            var paramArray = [];
-            for (var item in json) {
-                var val = json[item];
-                paramArray.push(item + "=" + val)
-            }
-            return "?" + paramArray.join("&");
-        }
+        urlSerialize: function(json = {}){
+			return "?" + json.keys().map(key=>`${key}=${json[key]}`).join("&");
+//             json = json || {};
+//             var paramArray = [];
+//             for (var item in json) {
+//                 var val = json[item];
+//                 paramArray.push(item + "=" + val)
+//             }
+//             return "?" + paramArray.join("&");
+        },
+		htmlItem: ["<", ">", " ", "<img />", "<img", "/>", "<script" "</script>"],
+		baseItem: ["&lt;", "&gt;", "&nbsp;", "&lt;img /&gt;", "&lt;img", "/&gt;", "&lt;script", "&lt/script&gt;" ],
+		htmlToBase: function(str){
+			return this.htmlItem.reduce(
+				(prev, next, index)=>(prev.replace(
+					new RegExp(next, "g"),
+					this.baseItem[index]
+				)),
+				str
+			);
+		},
+		baseToHtml: function(str){
+			return this.baseItem.reduce(
+				(prev, next, index)=>(prev.replace(
+					new RegExp(next, "g"),
+					this.htmlItem[index]
+				)),
+				str
+			);	
+		}
     }
 })(window, document);
