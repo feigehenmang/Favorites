ca96dd467573522ce691251098694f1023d80cc0
--global <--global>
2019-04-29 15:01:30 +0800

    'reactInit'

---
+++ page/blog/src/component/first.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+}
+
+export default class First extends React.Component<Props, object> {
+  render() {
+    const { name, enthusiasmLevel = 1 } = this.props;
+
+    if (enthusiasmLevel <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+      <div className="hello">
+        <div className="greeting">
+          First
+          Hello {name + getExclamationMarks(enthusiasmLevel)}
+        </div>
+      </div>
+    );
+  }
+}
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
---
+++ page/blog/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import First from "./component/first";
+ReactDOM.render(
+	<First name="TypeScript" enthusiasmLevel={10} />,
+	document.getElementById('root') as HTMLElement
+);
